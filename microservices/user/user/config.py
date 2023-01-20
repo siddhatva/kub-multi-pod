9806031d7e60ec99964407dc2fe8eecc6e6cfc72
Dilip Sharma <dilip.sharma@ishafoundation.org>
2023-01-20 20:41:21 +0530

    add inter process communication

--- microservices/user/user/config.py
+++ microservices/user/user/config.py
@@ -2,9 +2,12 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    folder:str = None
-    info_service_service_host:str = None
-    info_host:str = None
+    folder:str = "/data"
+    connection_way: str = "dns"
+    local: str = "localhost"
+    ip_address: str = "0.0.0.0"
+    info_service_service_host:str = ""
+    dns:str = "info-main"
     
 
 settings = Settings()
--- microservices/user/user/main.py
+++ microservices/user/user/main.py
@@ -3,10 +3,11 @@ import os
 from .config import settings
 import requests
 import json
+import logging
 
 
 app = FastAPI()
-
+logging.basicConfig(level=logging.DEBUG)
 
 async def directory_name():
     return settings.folder
@@ -19,10 +20,10 @@ async def home():
 async def info(data: str, directory: str = Depends(directory_name)):
     file_path = os.path.join(os.path.join(os.getcwd(), directory), "info.txt")
 
-    print("Writing data to file {}".format(file_path))
+    logging.info("Writing data to file {}".format(file_path))
     with open(file_path, "w") as f:
         f.write(data)
-        print("Wrote data to file")
+        logging.info("Wrote data to file")
     return {"Info": data}
 
 @app.get("/read")
@@ -30,30 +31,50 @@ async def info():
     data = None
     file_path = os.path.join(os.path.join(os.getcwd(), 'data'), "info.txt")
 
-    print("Reading data from file")
+    logging.info("Reading data from file")
 
     try:
         with open(file_path, "r") as f:
             data = f.read()
     except (IOError, OSError) as error:
-        print("Resource Not Found")
+        logging.info("Resource Not Found")
         return "Resource Not Found"
 
-    print("Read data from file")
+    logging.info("Read data from file")
     return {"Info": data}
 
 @app.get("/ip")
 async def ip():
-    data = None
-    url = "http://" + settings.info_host + "/read"
+    host = None
+
+    logging.info("Checking host type...")
+
+    if settings.connection_way == "local":
+        host = settings.local
+        logging.info("Host type is local '{}'.".format(host))
+    elif settings.connection_way == "ip":
+        host = settings.ip_address
+        logging.info("Host type is host '{}'.".format(host))
+    elif settings.connection_way == "host_name":
+        host = settings.info_service_service_host
+        logging.info("Host type is service host '{}'.".format(host))
+    elif settings.connection_way == "dns":
+        host = settings.dns
+        logging.info("Host type is dns '{}'.".format(host))
+    else:
+        host = "localhost"
+        logging.info("Host type is localhost '{}'.".format(host))
+
+    url = "http://" + host + ":5002" + "/read"
     
     print("Reading IP address from '{}'.".format(url))
+    logging.info("Reading IP address from '{}'.".format(url))
     response = requests.get(url=url)
 
     if response.status_code == 200:
-        print("IP Address read")
+        logging.info("IP Address read")
         return json.loads(response.text)
     else:
-        print("Failed to fetch IP address")
+        logging.info("Failed to fetch IP address")
         return {"Failed to fetch IP address"}
 
